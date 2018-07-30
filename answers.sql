eab1cc5578c24a90caedfe1e2b69c7f11c244b76
anne-other <anne.c.ince@gmail.com>
2018-07-30 16:40:09 +0100

    returning my name

--- answers.sql
+++ answers.sql
@@ -5,6 +5,7 @@ SELECT name FROM people;
 -- 3. Oops! Someone at CodeClan spelled Andrew's surname wrong! Change it to reflect the proper spelling ('Andrew Craggie' should be 'Andrew Craigie').
 UPDATE people SET name = 'Andrew Craigie' WHERE id = 1;
 -- 4. Return ONLY your name from the 'people' table.
+SELECT name FROM people WHERE name = 'Anne Ince'
 -- 5. The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
 -- 6. Create a new entry in the 'people' table with the name of one of the instructors.
 -- 7. Ben Affleck has decided to hijack our movie evening. Typical DC behaviour! Remove him from the table of people.
